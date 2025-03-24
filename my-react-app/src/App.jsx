3fc76020441a8e92596aeb0f579821de085ed4df
Nilesh Haldar <nileshhaldar98@gmail.com>
2025-03-24 15:57:34 +0530

    add

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1 style={{ fontSize: '3rem', color: '#333' }}>Nilesh Haldar</h1>
+    </div>
+  )
+}
+
+export default App
